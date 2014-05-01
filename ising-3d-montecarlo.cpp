b92fe156ec5a37ed5eff5d64633cee66c2a54e05
hideyuki <hmiyahara512@gmail.com>
2014-05-01 22:19:10 +0900

    2nd

--- ising-3d-montecarlo.cpp
+++ ising-3d-montecarlo.cpp
@@ -22,18 +22,19 @@ int main()
   double ene;
   double ene1;
   double ene2;
+  double temp;
+  double random;
 
   ofstream fout1("result1.txt");
   ofstream fout2("result2.txt");
   ofstream fout3("result3.txt");
   ofstream fout4("result4.txt");
+  ofstream fout5("result5.txt");
 
   std::mt19937 generator;
   generator.seed(time(NULL));
   std::normal_distribution<double> nordistribution(0.0, 0.030);
   std::uniform_real_distribution<double> unidistribution(0.0, 1.0);
-
-  double random;
 
     for (int i=0; i<Nx; i++)
       {
@@ -57,8 +58,10 @@ int main()
   ene = energy(sz);
   fout1 << 0 << ' ' << ene << endl;
 
-  for (int l=0; l<100; l++)
+  for (int l=0; l<1000; l++)
     {
+
+      temp=10.000*exp(-l/100);
 
       for (int i=0; i<Nx; i++)
         {
@@ -70,11 +73,13 @@ int main()
                   sz[i][j][k]=-1.0*sz[i][j][k];
                   ene2=energy(sz);
 
-                  fout4 << l << ' ' << ene1 << ' ' << ene2 << endl;
+                  fout4 << l << ' ' << ene1 << ' ' << ene2 << ' ' << ene2-ene1 << endl;
 
                   double uni=unidistribution(generator);
-                  if (uni < exp((ene2-ene1)/1.00))
+                  if (uni < 1/(1+exp(-(ene2-ene1)/1.00)))
+                    //if (ene2>ene1)
                     {
+                      fout5 << uni << ' ' << 1/(1+exp((ene2-ene1)/temp)) << endl;
                       sz[i][j][k]=-1.0*sz[i][j][k];
                     }
                 }
@@ -104,14 +109,38 @@ double energy(double sz[Nx][Ny][Nz])
 {
 
   double ene=0.0;
+  double J=1.0;
+  double H=1.0;
 
   for (int i=0; i<Nx-1; i++)
     {
+      for (int j=0; j<Ny; j++)
+        {
+          for (int k=0; k<Nz; k++)
+            {
+              ene=ene+J*sz[i][j][k]*sz[i+1][j][k];
+            }
+        }
+    }
+
+  for (int i=0; i<Nx; i++)
+    {
       for (int j=0; j<Ny-1; j++)
+        {
+          for (int k=0; k<Nz; k++)
+            {
+              ene=ene+J*sz[i][j][k]*sz[i][j+1][k];
+            }
+        }
+    }
+
+  for (int i=0; i<Nx; i++)
+    {
+      for (int j=0; j<Ny; j++)
         {
           for (int k=0; k<Nz-1; k++)
             {
-              ene=ene+sz[i][j][k]*sz[i][j][k];
+              ene=ene+J*sz[i][j][k]*sz[i][j][k+1];
             }
         }
     }
@@ -122,7 +151,7 @@ double energy(double sz[Nx][Ny][Nz])
         {
           for (int k=0; k<Nz; k++)
             {
-              ene=ene+0.1*sz[i][j][k];
+              ene=ene+H*sz[i][j][k];
             }
         }
     }
